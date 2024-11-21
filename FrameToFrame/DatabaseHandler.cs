361b5e4f9609a944b33857f7fe712ae193a8c996
boldisaron <79077046+boldisaron@users.noreply.github.com>
2024-11-21 10:33:56 +0100

    Add files via upload

---
+++ FrameToFrame/DatabaseHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+public class DatabaseHandler
+{
+    private static string _connectionString = "server=localhost;database=test;uid=root;pwd='';";
+
+    // Statikus függvény, amely végrehajtja az SQL utasítást és megjeleníti az eredményt egy új formban, ha van eredmény
+    public static bool ExecuteAndDisplay(string sqlQuery)
+    {
+
+        bool res = false;
+        try
+        {
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows) // Ellenőrzi, hogy van-e legalább egy sor eredmény
+                        {
+                            // Eredmény begyűjtése egy string formájában
+                            string result = "";
+
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    result += reader.GetName(i) + ": " + reader.GetValue(i).ToString() + "\n";
+                                }
+                                result += "\n";
+                            }
+
+                            Debug.WriteLine("----------------" + sqlQuery + "---------------");
+                            // Új form megnyitása az eredményekkel
+                            // ResultForm resultForm = new ResultForm(result);
+                            //FormUtilities.CenterForm(resultForm);
+                            //resultForm.ShowDialog(); // Modal form megjelenítése
+
+                            res = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nincs eredmény a lekérdezéshez.", "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Hiba történt: " + ex.Message, "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        return res;
+    }
+}
+
+// Az új form, amely megjeleníti az SQL lekérdezés eredményét
+public class ResultForm : Form
+{
+    private TextBox resultTextBox;
+
+    public ResultForm(string resultData)
+    {
+        this.Text = "Lekérdezés Eredménye";
+        this.Size = new System.Drawing.Size(400, 300);
+
+        resultTextBox = new TextBox();
+        resultTextBox.Multiline = true;
+        resultTextBox.Dock = DockStyle.Fill;
+        resultTextBox.Text = resultData;
+        resultTextBox.ReadOnly = true;
+
+        this.Controls.Add(resultTextBox);
+    }
+}
---
+++ FrameToFrame/FormUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class FormUtilities // Az osztály nem statikus
+{
+    // Statikus metódus, amely a megadott formot középre igazítja
+    public static void CenterForm(Form form)
+    {
+        if (form == null)
+        {
+            throw new ArgumentNullException(nameof(form), "A form paraméter nem lehet null.");
+        }
+
+        // A képernyő méretének lekérdezése
+        Rectangle screenBounds = Screen.FromControl(form).Bounds;
+
+        // A form pozíciójának kiszámítása a képernyő közepére
+        int x = (screenBounds.Width - form.Width) / 2;
+        int y = (screenBounds.Height - form.Height) / 2;
+
+        // Form pozíciójának beállítása
+        form.StartPosition = FormStartPosition.Manual;
+        form.Location = new Point(x, y);
+    }
+}
